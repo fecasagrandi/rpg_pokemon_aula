c0984d08bc01506d8d3ab15d8a89080bae57b369
Felipe Casagrandi <fcasagrandi38@gmail.com>
2026-09-20 16:09:45 -0300

    Criando método com heranças

--- pokemon.py
+++ pokemon.py
@@ -1,19 +1,27 @@
 class Pokemon:
-    def __init__(self, tipo, especie):
+
+    def __init__(self, tipo, especie, level=1, nome=None):
         self.tipo = tipo
         self.especie = especie
+        self.level = level
+
+        if nome:
+            self.nome = nome
+        else:
+            self.nome = especie
 
     def __str__(self):
-        return "{} ({})".format(self.especie, self.tipo)
+        return "{}({})".format(self.nome, self.level)
 
     def atacar(self, pokemon):
-        print("{} atacou o {}".format(self.especie, pokemon.especie))
+        print("{} atacou {}!".format(self, pokemon))
 
-    def defender(self, pokemon):
-        print("{} defendeu o `{}".format(pokemon.especie, pokemon.tipo))
+class PokemonEletrico(Pokemon):
+    def atacar(self, pokemon):
+        print("{} Lançou um raio do trovão em: {}!".format(self, pokemon))
 
-meu_pokemon = Pokemon("Fogo", "Charmander")
-pokemon_amigo = Pokemon("Elétrico", "Pikachu")
+meu_pokemon = PokemonEletrico("Elétrico", "Pikachu")
+amigo_pokemon = Pokemon("Fogo", "Charmander")
 
-meu_pokemon.defender(pokemon_amigo)
-
+meu_pokemon.atacar(amigo_pokemon)
+amigo_pokemon.atacar(meu_pokemon)
